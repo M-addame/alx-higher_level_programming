b720cbc991a02adfef69398414e638f95347ce42
Adam <simoaddame17@gmail.com>
2024-07-06 17:06:19 +0200

    all

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const ParentSquare = require('./5-square.js');
+
+class Square extends ParentSquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    for (let i = 0; i < this.height; i++) {
+      let square = '';
+      for (let j = 0; j < this.width; j++) {
+        square += c;
+      }
+      console.log(square);
+    }
+  }
+}
+
+module.exports = Square;
---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let n = 0;
+
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      n++;
+    }
+  }
+  return (n);
+};
